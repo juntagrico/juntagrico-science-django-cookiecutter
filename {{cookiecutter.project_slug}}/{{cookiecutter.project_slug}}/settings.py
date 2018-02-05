8a1629c1f6543b8b8777c9992d5bfbdfe5a6f52a
David Simmen <david.simmen@bluewin.ch>
2018-02-05 12:27:28 +0100

    inital

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -0,0 +1,201 @@
+"""
+Django settings for {{cookiecutter.project_slug}} project.
+"""
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '8cd-j&jo=-#ecd1jjulp_s*7y$n4tad(0d_g)l=6@n^r8fg3rn'
+
+DEBUG = os.environ.get("JUNTAGRICO_DEBUG", 'True')=='True'
+
+ALLOWED_HOSTS = ['{{cookiecutter.admin_portal_url}}', 'localhost',]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'juntagrico',
+    'impersonate',
+    '{{cookiecutter.project_slug}}',
+]
+
+ROOT_URLCONF = '{{cookiecutter.project_slug}}.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('JUNTAGRICO_DATABASE_ENGINE','django.db.backends.sqlite3'), 
+        'NAME': os.environ.get('JUNTAGRICO_DATABASE_NAME','{{cookiecutter.project_slug}}.db'), 
+        'USER': os.environ.get('JUNTAGRICO_DATABASE_USER'), #''junatagrico', # The following settings are not used with sqlite3:
+        'PASSWORD': os.environ.get('JUNTAGRICO_DATABASE_PASSWORD'), #''junatagrico',
+        'HOST': os.environ.get('JUNTAGRICO_DATABASE_HOST'), #'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': os.environ.get('JUNTAGRICO_DATABASE_PORT', False), #''', # Set to empty string for default.
+    }
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+                'juntagrico.personalisation.loaders.personal_directories.Loader'
+            ],
+            'debug' : True
+        },
+    },
+]
+
+WSGI_APPLICATION = '{{cookiecutter.project_slug}}.wsgi.application'
+
+
+LANGUAGE_CODE = 'de_CH'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
+
+DATE_INPUT_FORMATS =['%d.%m.%Y',]
+
+AUTHENTICATION_BACKENDS = (
+    'juntagrico.util.auth.AuthenticateWithEmail',
+    'django.contrib.auth.backends.ModelBackend'
+)
+
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+EMAIL_HOST = os.environ.get('JUNTAGRICO_EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('JUNTAGRICO_EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('JUNTAGRICO_EMAIL_PASSWORD')
+EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25' ))
+EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'False')=='True'
+EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False')=='True'
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+
+WHITELIST_EMAILS = []
+
+def whitelist_email_from_env(var_env_name):
+    email = os.environ.get(var_env_name)
+    if email:
+        WHITELIST_EMAILS.append(email.replace('@gmail.com', '(\+\S+)?@gmail.com'))
+
+
+if DEBUG is True:
+    for key in os.environ.keys():
+        if key.startswith("JUNTAGRICO_EMAIL_WHITELISTED"):
+            whitelist_email_from_env(key)
+            
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+IMPERSONATE_REDIRECT_URL = "/my/profile"
+
+LOGIN_REDIRECT_URL = "/my/home"
+
+"""
+    File & Storage Settings
+"""
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+MEDIA_ROOT = 'media'
+
+MEMBER_STRING = "Mitglied"
+MEMBERS_STRING = "Mitglieder"
+ASSIGNMENT_STRING = "Mitglied"
+ASSIGNMENTS_STRING = "Arbeitseinsätze"
+ORGANISATION_NAME = "{{cookiecutter.organisation_name}}"
+ORGANISATION_LONG_NAME = "{{cookiecutter.organisation_name}}"
+ORGANISATION_ADDRESS = {"name":"{{cookiecutter.organisation_name}}", 
+            "street" : "{{cookiecutter.street}}",
+            "number" : "{{cookiecutter.number}}",
+            "zip" : "{{cookiecutter.zip}}",
+            "city" : "{{cookiecutter.city}}",
+            "extra" : "{{cookiecutter.extra}}"}
+ORGANISATION_PHONE =''
+ORGANISATION_BANK_CONNECTION = {"PC" : "{{cookiecutter.PC}}",
+            "IBAN" : "{{cookiecutter.IBAN}}",
+            "BIC" : "{{cookiecutter.BIC}}",
+            "NAME" : "{{cookiecutter.NAME}}",
+            "ESR" : "{{cookiecutter.ESR}}"}
+INFO_EMAIL = "{{cookiecutter.info_email}}"
+SERVER_URL = "{{cookiecutter.server_url}}"
+ADMINPORTAL_NAME = "{{cookiecutter.admin_portal_name}}"
+ADMINPORTAL_SERVER_URL = "{{cookiecutter.admin_portal_url}}"
+BUSINESS_REGULATIONS = "/static/docs/business_regulations.pdf"
+BYLAWS = "/static/docs/bylaws.pdf"
+STYLE_SHEET = "/static/css/personal.css"
+BOOTSTRAP = "/static/external/bootstrap-3.3.1/css/bootstrap.min.css"
+FAVICON = "/static/img/favicono.ico"
+FAQ_DOC = "/static/doc/fac.pdf"
+EXTRA_SUB_INFO = "/static/doc/extra_sub_info.pdf"
+ACTIVITY_AREA_INFO = "/static/doc/activity_area_info.pdf"
+SHARE_PRICE = "{{cookiecutter.share_price}}"
+PROMOTED_JOB_TYPES = []
+PROMOTED_JOBS_AMOUNT = 2
+DEPOT_LIST_GENERATION_DAYS = [1,2,3,4,5,6,7]	
+BILLING = False
+BUSINESS_YEAR_START = {"day":1, "month":1}
+BUSINESS_YEAR_CANCELATION_MONTH = 10
+IMAGES = {'status_100': '/static/img/status_100.png',
+            'status_75': '/static/img/status_75.png',
+            'status_50': '/static/img/status_50.png',
+            'status_25': '/static/img/status_25.png',
+            'status_0': '/static/img/status_0.png',
+            'single_full': '/static/img/single_full.png',
+            'single_empty': '/static/img/single_empty.png',
+            'single_core': '/static/img/single_core.png',
+            'core': '/static/img/core.png'}
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+EMAILS = {
+    'welcome': 'mails/welcome_mail.txt',
+    'co_welcome': 'mails/welcome_added_mail.txt',
+    'password': 'mails/password_reset_mail.txt',
+    'j_reminder': 'mails/job_reminder_mail.txt',
+    'j_canceled': 'mails/job_canceled_mail.txt',
+    'confirm': 'mails/confirm.txt',
+    'j_changed': 'mails/job_time_changed_mail.txt',
+    'j_signup': 'mails/job_signup_mail.txt',
+    'd_changed': 'mails/depot_changed_mail.txt',
+    's_canceled': 'mails/subscription_canceled_mail.txt',
+    'b_share': 'mails/bill_share.txt',
+    'b_sub': 'mails/bill_sub.txt',
+    'b_esub': 'mails/bill_extrasub.txt'
+}
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for {{cookiecutter.project_slug}} project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_slug}}.settings")
+
+application = get_wsgi_application()
+
